77f2e15b268d330df484b448b9d0f33d5cd073b3
Megha Modi <meghamodi007@gmail.com>
2026-05-26 20:21:16 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    userarg = input()
-    print(f"{userarg}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        
+        userarg = input()
+        print(f"{userarg}: command not found")
 
 
 if __name__ == "__main__":
