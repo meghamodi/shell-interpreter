be56b878e55f02c305935a2033e6a02a9ba71265
Megha Modi <meghamodi007@gmail.com>
2026-06-01 15:44:57 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import sys
-
+import os
 
 def main():
+    PATH = os.environ["PATH"]
     while True:
         sys.stdout.write("$ ")
         userarg = input()
+        
         if userarg == "exit":
             break
         elif userarg[:5] == "echo ":
@@ -12,8 +14,25 @@ def main():
         elif userarg[:5] == "type ":
             if userarg[5:] == "echo" or userarg[5:] == "exit" or userarg[5:] == "type" :
                 print(f"{userarg[5:]} is a shell builtin")
+                
             else:
-                print(f"{userarg[5:]}: not found")
+                cmd = userarg[5:]
+                for dir in PATH.split(":"):
+                    # change dir, then search for the command, if not go to next dir
+                    try:
+                        entries= os.listdir(dir)
+                        if cmd in entries:
+                            fullPath = os.path.join(dir,cmd)
+                            if os.access(fullPath,os.X_OK):
+                                print(f"{cmd} is {fullPath}")
+                                break
+                        
+                    except:
+                        continue
+                else:
+                    # only runs if loop exhausted all dirs without break
+                    print(f"{cmd}: not found")
+
         else:
             print(f"{userarg}: command not found")
 
